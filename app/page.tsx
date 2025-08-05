4d353d2f573e78b8fcf05f8ba078e835f80b1771
shazab-saifi <shazebsaifi92@gmail.com>
2025-08-05 16:41:57 +0530

    Added metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,31 @@
 import Landing from "@/components/Landing";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Shazab Saifi",
+  description:
+    "I'm a design engineer who crafts user interfaces and writes code. I blend design and development to create seamless, engaging digital experiences.",
+  openGraph: {
+    images: [
+      {
+        url: "/OgImage.svg",
+        alt: "Shazab Saifi OG Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Shazab Saifi",
+    description:
+      "I'm a design engineer who crafts user interfaces and writes code. I blend design and development to create seamless, engaging digital experiences.",
+    images: [
+      {
+        url: "/OgImage.svg",
+        alt: "Shazab Saifi OG Image",
+      },
+    ],
+  },
+};
 
 export default function Home() {
   return <Landing />;
